43d6a8dba5ddb4a1766b3b507b11be0e5dce0b99
ThatGuySob <101882926+ThatGuySob@users.noreply.github.com>
2024-03-13 10:53:52 +0000

    Add files via upload

--- Lab7-9/locationfinder.c
+++ Lab7-9/locationfinder.c
@@ -1,30 +1,61 @@
-#pragma config(Sensor, S1,     Light,          sensorEV3_Color)
-#pragma config(Sensor, S2,     touchRight,     sensorEV3_Touch)
-#pragma config(Sensor, S3,     Gyro,           sensorEV3_Gyro)
-#pragma config(Sensor, S4,     ultraSensor,    sensorEV3_Ultrasonic)
-#pragma config(Motor,  motorA,          GyroMotor,     tmotorEV3_Large, PIDControl, encoder)
-#pragma config(Motor,  motorB,          Left,          tmotorEV3_Large, PIDControl, driveLeft, encoder)
-#pragma config(Motor,  motorC,          Right,         tmotorEV3_Large, PIDControl, driveRight, encoder)
-//reassigning sensors and motors using built-in pragma setup
+void turnGyro(long power, long turnDegree);//the speed of the motors during the turn, how many degrees motorC it will turn
 
+void drive(long nMotorRatio, long dist, long power);// the ratio of the motors, how far in centimatres to travel, how fast to travel
+
+#define Centimetre 20.93
 
 task main()
 {
 
-	turn90(10, 90);
+	int count = 0;
+	int notblack=0;
+	int dist=10;
 
-}//end program
-
-void turn90(long power, long turnDegree)
-{
-	resetGyro(sensorEV3_Gyro);
-	while(getGyroDegrees(sensorEV3_Gyro)-1<turnDegree-3)//the minus is to account for the lag
+	while (!SensorValue(sensorEV3_Touch))
 	{
-		displayCenteredBigTextLine(6,"Gyro:%d",getGyroDegrees(sensorEV3_Gyro));//debbugging text
+		setMotorSpeed(motorB, 30);
+		setMotorSpeed(motorC, 30);
+
+		if (SensorValue(sensorEV3_Color) < 40 && (notblack==1))
+		{
+			count++;
+			displayCenteredBigTextLine(3, "Black Lines: %d", count);
+			notblack=0;
+		}
+		if (SensorValue(sensorEV3_Color) > 40)
+		{
+			notblack=1;
+		}
+
+	}
+
+	drive(0, dist, 20); // travel foward dist cm at 20 power
+	turnGyro(10, 90); //power 10, turn motorC 90*
+
+}
+
+void turnGyro(long power, long turnDegree) //the speed of the motors during the turn, how many degrees motorC it will turn
+{
+	resetGyro(S3);
+	while(getGyroDegrees(S3) < turnDegree)//the minus is to account for the lag
+	{
+		displayCenteredBigTextLine(6,"sensorEV3_Gyro:%d",getGyroDegrees(S3));//debbugging text
 		setMotorSpeed(motorB,power);//motors are turning
 		setMotorSpeed(motorC,-power);
 	}
 	setMotorSpeed(motorB,0);//stopping the motors after
 	setMotorSpeed(motorC,0);
 
+}
+
+void drive(long nMotorRatio, long dist, long power) // the ratio of the motors, how far in centimatres to travel, how fast to travel
+{
+	int rotations = (Centimetre * dist);
+
+	resetMotorEncoder(motorB);
+	resetMotorEncoder(motorC);
+
+	setMotorSyncEncoder(motorB, motorC, nMotorRatio, rotations, power);
+
+	waitUntilMotorStop(motorC);
 }
